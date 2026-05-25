5893974d92ca2768f03b74f91effe5090d4e888f
ansloki-art <ansloki@gmail.com>
2026-05-25 14:21:59 +0700

    fix: migration chain

--- backend/alembic/versions/a1b2c3d4e5f6_add_payment_fields_to_bookings.py
+++ backend/alembic/versions/a1b2c3d4e5f6_add_payment_fields_to_bookings.py
@@ -9,7 +9,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision = 'a1b2c3d4e5f6'
-down_revision = 'b8b0e16dbc85'
+down_revision = '1dd93d740cea'
 branch_labels = None
 depends_on = None
 
